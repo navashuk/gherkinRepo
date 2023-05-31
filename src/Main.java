dda3a9dfe55b5a5451b3d3bdf67c8a0f33bb74b5
navashuk <mr.shukroew@gmail.com>
2023-05-30 20:16:34 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello world!");
+
+    for (int i=0;i<11;i++){
+        System.out.println("hello");
+        Thread.sleep(2000);
+    }
+
+
+    }
+}
